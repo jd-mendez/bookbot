110879c4f6a39e2d251a4322b67bc9e5012d3e75
jd-mendez <mendez.jd@yahoo.com>
2025-03-20 15:18:27 -0500

    final version of the bookbot

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from stats import (get_book_text, get_num_words, count_characters, character_dict_to_sorted_list)
+import sys
+if len (sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+book_path = sys.argv[1]
 
+from stats import (get_num_words, count_characters, character_dict_to_sorted_list)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
 def main():
-    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = count_characters(text)
     chars_sorted_list = character_dict_to_sorted_list(chars_dict)
     print_report(book_path, num_words, chars_sorted_list)
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
 
     
 def print_report(book_path, num_words, chars_sorted_list):
--- stats.py
+++ stats.py
@@ -1,8 +1,4 @@
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-    
 def get_num_words(text):
     words = text.split()
     return len(words)
